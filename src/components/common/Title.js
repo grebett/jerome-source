0f35b5faa434603898bd7cd3e95227f8dd752fca
Grégory Betton <gregory.betton@gmail.com>
2017-04-05 22:25:34 +0200

    mdq: Title

--- src/components/common/Title.js
+++ src/components/common/Title.js
@@ -1,24 +1,33 @@
 import React from 'react'
+import Radium from 'radium'
 
 class Title extends React.Component {
   render() {
+    const sizes = {
+      h1: 72,
+      h2: 30,
+      h3: 26,
+      h4: 22,
+      h5: 17,
+      h6: 12
+    }
+    const size = {
+      '1440px': sizes[this.props.size] + 'px',
+      '1280px': sizes[this.props.size] * 0.8 + 'px',
+      '1024px': sizes[this.props.size] * 0.7 + 'px'
+    }
     const titleStyles = {
       fontFamily: 'Kano',
       color: '#4a4a4a',
       fontWeight: 'normal',
-      margin: '0px'
+      margin: '0px',
+      fontSize: size['1440px'],
+      lineHeight: size['1440px']
     }
-    const sizes = {
-      h1: '72px',
-      h2: '30px',
-      h3: '26px',
-      h4: '22px',
-      h5: '17px',
-      h6: '12px'
+    titleStyles['@media (max-width: 1439px)'] = {
+      fontSize: size['1280px'],
+      lineHeight: size['1280px']
     }
-
-    titleStyles.fontSize = sizes[this.props.size]
-    titleStyles.lineHeight = sizes[this.props.size]
 
     // could check if this.props.size is a valid hx element
     return (
@@ -27,4 +36,4 @@ class Title extends React.Component {
   }
 }
 
-export default Title
+export default Radium(Title)
